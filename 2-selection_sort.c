12d484bb91994171ee8e227a8e73a01d0493e48e
Dustin Davis <davisdustin328@gmail.com>
2022-12-19 18:11:34 -0600

    Function using the selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,31 @@
+#include "sort.h"
 
+/**
+ * selection_sort - sorts an array of numbers in selection sort algorithm
+ * @array: input array of integers
+ * @size: size of the array
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, p, m;
+
+	for (i = 0; i < size; i++)
+	{
+		m = i;
+		for (p = i + 1; p < size; p++)
+		{
+			if (array[m] > array[p])
+				m = p;
+		}
+
+		if (i != m)
+		{
+			tmp = array[i];
+			array[i] = array[m];
+			array[m] = tmp;
+			print_array(array, size);
+		}
+	}
+}
